3f6fbad274906cbcfa58b735fc9429d0a40d28ff
deepikadwivedi <deepika.dwivedi@gmail.com>
2021-09-21 00:03:51 -0700

    Docs and result fixes
    Returning the result wrapped in a JSON object
    Fixed documentation page

--- webserver.py
+++ webserver.py
@@ -2,10 +2,12 @@ from flask import Flask,render_template, jsonify, request, abort
 from flask_restful import Resource
 import sys
 import re
+import ipaddress
+
 
 app = Flask(__name__)
 
-
+    
 class LogRequest():
     def __init__(self, request):
         self.filtered_request = False
@@ -22,22 +24,23 @@ class LogRequest():
         if self.query:
             self.compiled_query = re.compile(self.query)
             self.filtered_request = True
-
+        
         if request.args.get('numlines'):
             self.numlines = int(request.args.get('numlines'))
             if self.numlines < 1:
                 raise ValueError('numlines must be a positive integer')
         else:
             self.numlines = 20
+            
         self.file = open("/var/log/" + self.filename, "rb")
 
     def tail(self):
         lines_to_go = self.total_lines_remaining + 1
-        blocks = [] # blocks of size BLOCK_SIZE, in reverse order starting                                                                
+        blocks = [] # blocks of size BLOCK_SIZE, in reverse order starting
         hit_start = False
-        # from the end of the file                                                                                                        
+        # from the end of the file
         while lines_to_go > 0 and self.last_read_pos != 0:
-            # read the last block we haven't yet read                                                                                     
+            # read the last block we haven't yet read
             self.file.seek(self.next_read_pos, 0)
             read_size = self.last_read_pos - self.next_read_pos
             blocks.append(self.file.read(read_size).decode('utf-8'))
@@ -45,19 +48,19 @@ class LogRequest():
             lines_to_go -= lines_found
             self.last_read_pos = self.next_read_pos
             self.next_read_pos = 0 if self.last_read_pos - self.BLOCK_SIZE < 0 else self.last_read_pos - self.BLOCK_SIZE
-
+            
         all_read_text = ''.join(reversed(blocks))
 
-        #Now that we have found the appropriate number of lines, seek to the prior newline                                                
-        #since we might have the tail end of the first of our lines, do this if we havent hit the start                                   
+        #Now that we have found the appropriate number of lines, seek to the prior newline
+        #since we might have the tail end of the first of our lines, do this if we havent hit the start
         if self.last_read_pos != 0:
             self.file.seek(self.next_read_pos, 0)
             read_size = self.last_read_pos - self.next_read_pos
             block = self.file.read(read_size).decode('utf-8')
             prev_newline_ptr = block.rfind(u'\n')
             all_read_text = block[prev_newline_ptr + 1:] + all_read_text
-
-            self.last_read_pos -= prev_newline_ptr
+            
+            self.last_read_pos -= prev_newline_ptr 
             self.next_read_pos = 0 if self.last_read_pos - self.BLOCK_SIZE < 0 else self.last_read_pos - self.BLOCK_SIZE
 
         return all_read_text.splitlines()
@@ -82,14 +85,20 @@ class LogRequest():
             flist.extend(self.result)
             self.result = flist
             lines = self.tail()
-            
-            
-    class Logger(Resource):
+
+class Logger(Resource):
+    @app.route('/')
+    def get_root():
+        return render_template('index.html')
+    @app.route('/docs')
+    def get_docs():
+        return render_template('docs.html')
+
     @app.route("/logfiles")
-    # View a log file                                                                                                                     
-    # Query params:                                                                                                                       
-    # filename=name.log                                                                                                                   
-    # numlines=100                                                                                                                        
+    # View a log file
+    # Query params:
+    # filename=name.log
+    # numlines=100
     def search_logfile():
         try:
             req = LogRequest(request)
@@ -102,8 +111,8 @@ class LogRequest():
         except re.error:
             abort(400, "Invalid regular expression")
         req.run()
-        return jsonify(req.result)
-
+        return jsonify({"search_result": req.result})
+            
 
 if __name__ == "__main__":
     app.run(debug=True)
